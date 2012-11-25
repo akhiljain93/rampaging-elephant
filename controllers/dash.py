e593ee4425bfdb150db9c62ec78a03a0704edc2a
Shivanker Goel <shivanker.goel@gmail.com>
2012-11-25 15:29:52 +0530

    trending bugfix

--- controllers/dash.py
+++ controllers/dash.py
@@ -137,32 +137,39 @@ def t_topics():
             filt = request.args[0]
             table = db.lhourwise
     if not filt: # if filter is not applied
-        latest = db(table.id>0).select(table.time.max().with_alias('time'),limitby=(0,1)).first().time - 3600*1000
-        day1 = db(table.time==latest).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time')
-        dayhist = db(table.time>(latest-4*60*60*24*1000))(table.time<latest).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time', orderby=table.time)
+        latest = db(table.id>0).select(table.time.max().with_alias('time'),limitby=(0,1)).first().time - 2*3600*1000
+        day1 = db(table.time>=latest)(table.time < latest+2*3600*1000).select(table.topic.with_alias('topic'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic')
+        dayhist = db(table.time>=(latest-3*60*60*24*1000))(table.time<latest).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time', orderby=table.time)
     elif cluswise: # if cluster id is provided
-        latest = db(table.cledge==db.cledges.id)((db.cledges.cluster1==filt)|(db.cledges.cluster2 == filt)).select(table.time.max().with_alias('time'),limitby=(0,1)).first().time - 3600*1000
-        day1 = db(table.time==latest)(table.cledge==db.cledges.id)((db.cledges.cluster1==filt)|(db.cledges.cluster2 == filt)).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time')
-        dayhist = db(table.time>(latest-4*60*60*24*1000))(table.time<latest)(table.cledge==db.cledges.id)((db.cledges.cluster1==filt)|(db.cledges.cluster2 == filt)).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time')
+        latest = db(table.cledge==db.cledges.id)((db.cledges.cluster1==filt)|(db.cledges.cluster2 == filt)).select(table.time.max().with_alias('time'),limitby=(0,1)).first().time - 2*3600*1000
+        day1 = db(table.time>=latest)(table.time < latest+2*3600*1000)(table.cledge==db.cledges.id)((db.cledges.cluster1==filt)|(db.cledges.cluster2 == filt)).select(table.topic.with_alias('topic'), table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic')
+        dayhist = db(table.time>=(latest-3*60*60*24*1000))(table.time<latest)(table.cledge==db.cledges.id)((db.cledges.cluster1==filt)|(db.cledges.cluster2 == filt)).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time', orderby=table.time)
     else: # if location is provided
-        latest = db(table.locedge==db.locedges.id)((db.locedges.loc1==filt)|(db.locedges.loc2==filt)).select(table.time.max().with_alias('time'),limitby=(0,1)).first().time - 3600*1000
-        day1 = db(table.time==latest)(table.locedge==db.locedges.id)((db.locedges.loc1==filt)|(db.locedges.loc2==filt)).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time')
-        dayhist = db(table.time>(latest-4*60*60*24*1000))(table.time<latest)(table.locedge==db.locedges.id)((db.locedges.loc1==filt)|(db.locedges.loc2==filt)).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time')
+        latest = db(table.locedge==db.locedges.id)((db.locedges.loc1==filt)|(db.locedges.loc2==filt)).select(table.time.max().with_alias('time'),limitby=(0,1)).first().time - 2*3600*1000
+        day1 = db(table.time>=latest)(table.time < latest+2*3600*1000)(table.locedge==db.locedges.id)((db.locedges.loc1==filt)|(db.locedges.loc2==filt)).select(table.topic.with_alias('topic'), table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic')
+        dayhist = db(table.time>=(latest-3*60*60*24*1000))(table.time<latest)(table.locedge==db.locedges.id)((db.locedges.loc1==filt)|(db.locedges.loc2==filt)).select(table.topic.with_alias('topic'),table.time.with_alias('time'),table.volume.sum().with_alias('volumesum'), groupby=str(table)+'.topic,'+str(table)+'.time', orderby=table.time)
     score = []
     for x in day1:
         arr = []
         currentdate = latest-3*60*60*24*1000
         for y in dayhist:
             if (x.topic==y.topic):
-                for z in range((y.time - currentdate)/(60*60*24*1000)):
+                for z in range((y.time - currentdate)/(60*60*1000)):
                     arr.append(0)
                 arr.append(y.volumesum)
-                currentdate = y.time + 60*60*24*1000
-        if len(arr) > 0:
-            score.append({'topic':x.topic, 'score':int(zscore(x.volumesum,arr)*100)/100.0})
+                currentdate = y.time + 60*60*1000
+        while len(arr) < 72:
+            arr.append(0)
+        arrnew = []
+        for i in range(0,len(arr),2):
+            arrnew.append(arr[i] + arr[i+1])
+        arrnew.append(x.volumesum)
+        if len(arrnew) > 0:
+            score.append({'topic':x.topic, 'score':int(zscore(x.volumesum, arrnew[:-1])*100)/100.0, 'diff': sum(arrnew[-12:])-sum(arrnew[-24:-12])})
     score.sort(sortthis)
-    score = map(lambda x: dict(topic=x['topic'],score=(x['score'])),score)
-    return dict(abeo=score, latest = latest)
+    diffsortedTopics = map(lambda x: x[1], sorted(map(lambda x: (x['diff'], x['topic']),score),reverse=True))
+    sortedScores = map(lambda x: x['score'], score)
+    return dict(topics = diffsortedTopics, scores = sortedScores, latest = latest)
 
 # Motion Chart Helper Page: returns data required to create motion chart
 def motion():
